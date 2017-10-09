8965a9cc08f0cef5abfbea6f55e9f5a0b2d9a34d
Malachi Hudson <malachi.hudson.8@gmail.com>
2017-10-09 19:48:39 +0000

    Tried fixing an error: Name Error in movies#index, not sure what is going on.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,12 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  
+  # add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    
+  end
 end
